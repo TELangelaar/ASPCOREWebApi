2d8be7d6ca2460110d05cbde8169bc06a611897f
Thijme Langelaar <Thijme.Langelaar@gmail.com>
2021-12-22 14:23:01 +0100

    Dependency Injection & Inversion of Control implemented

---
+++ CityInfo/CityInfo.API/Services/CloudMailService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CityInfo.API.Services
+{
+    public class CloudMailService : IMailService
+    {
+        private readonly ILogger<CloudMailService> _logger;
+        private string _mailTo = "admin@mycompany.com";
+        private string _mailFrom = "noreply@mycompany.com";
+
+        public CloudMailService(ILogger<CloudMailService> logger)
+        {
+            _logger = logger;
+        }
+        public void Send(string subject, string message)
+        {
+            _logger.LogDebug("Mail from {_mailFrom} to {_mailTo}, using {mailService}.", _mailFrom, _mailTo, GetType().Name);
+            _logger.LogDebug("Subject: {subject}", subject);
+            _logger.LogDebug("Message: {message}", message);
+        }
+    }
+}
---
+++ CityInfo/CityInfo.API/Services/LocalMailService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CityInfo.API.Services
+{
+    public class LocalMailService : IMailService
+    {
+        private readonly ILogger<LocalMailService> _logger;
+        private string _mailTo = "admin@mycompany.com";
+        private string _mailFrom = "noreply@mycompany.com";
+
+        public LocalMailService(ILogger<LocalMailService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Send(string subject, string message)
+        {
+            _logger.LogDebug("Mail from {_mailFrom} to {_mailTo}, using {mailService}.", _mailFrom, _mailTo, GetType().Name);
+            _logger.LogDebug("Subject: {subject}", subject);
+            _logger.LogDebug("Message: {message}", message);
+        }
+    }
+}
